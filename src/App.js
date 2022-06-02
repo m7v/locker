36220c8debd05a61fd8b750f568fed833836f550
Andrew Mikhailov <a.mikhaylov@2gis.ru>
2022-06-02 21:27:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import {useState} from "react";
+import './App.css';
+import On from "./on";
+import Off from "./off";
+
+function rand() { // min and max included
+    return Math.floor(Math.random() * (2) + 0)
+}
+
+function App() {
+  const [matrix, setMatrix] = useState([
+      [rand(), rand(), rand(), rand()],
+      [rand(), rand(), rand(), rand()],
+      [rand(), rand(), rand(), rand()],
+      [rand(), rand(), rand(), rand()],
+  ]);
+
+  const handleClick = (i, j) => {
+      let newMatrix = [...matrix];
+      newMatrix = newMatrix.map((arr, iIdx) => {
+          return arr.map((item, jIdx) => {
+              if (jIdx === j) {
+                  return item === 0 ? 1 : 0
+              }
+              return item;
+          });
+      });
+      newMatrix[i] = newMatrix[i].map((item, jIdx) => {
+          return newMatrix[i][jIdx] === 0 ? 1 : 0;
+      });
+      newMatrix[i][j] = newMatrix[i][j] === 0 ? 1 : 0
+      setMatrix(newMatrix);
+  };
+
+  return (
+    <div className="container">
+        <div className="locker" style={{ width: matrix[0].length * 68 }}>
+            <div className="signal">
+                {matrix.map((arr, index) => (
+                    <div className="signal-wrap">
+                        {arr.every((i) => i === 1) ? <div className="green" /> : <div className="red" />}
+                    </div>
+                ))}
+            </div>
+            {matrix.map((arr, i) => (
+                <tr key={i} className="row">
+                    {arr.map((item, j) => (
+                        <td key={j} className="column"><button className="button" onClick={() => handleClick(i, j)}>{item === 1 ? <On /> : <Off />}</button></td>
+                    ))}
+                </tr>
+            ))}
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/on.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+const On = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 485 485"
+        style={{
+            enableBackground: "new 0 0 485 485",
+        }}
+        xmlSpace="preserve"
+        fill={"#2e7d32"}
+        {...props}
+    >
+        <path d="M356.25 113.75h-227.5C57.757 113.75 0 171.507 0 242.5s57.757 128.75 128.75 128.75h227.5c70.993 0 128.75-57.757 128.75-128.75s-57.757-128.75-128.75-128.75zm0 227.5c-54.451 0-98.75-44.299-98.75-98.75s44.299-98.75 98.75-98.75S455 188.049 455 242.5s-44.299 98.75-98.75 98.75z" />
+    </svg>
+)
+
+export default On
